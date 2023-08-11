d702a822a6ceec10e8b0f32a20d55c0048b144e3
kta-wd <xaydagolds@gmail.com>
2023-08-11 22:07:38 +0700

    add feature

---
+++ feature.cs
@@ -0,0 +1,12 @@
+
+public async Task<object> MyMethodAsync(string parameter)
+{
+    
+}
+
+public string Name { get; private set; }
+
+public async Task<IActionResult> MyMethodAsync(string parameter)
+{
+    
+}
